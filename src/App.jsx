e8ea115c0ba14104b9b7392d5e8a68113dd78d34
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-03-08 12:00:00 -0500

    late

--- src/App.jsx
+++ src/App.jsx
@@ -114,12 +114,3 @@ function App() {
 }
 
 export default App
-
-
-// Do: Build a Hitlist using React (JS or TS)
-// Definition of Done:
-// Companies should be saved in JSON Server.
-// User should be able to add companies.
-// User should be able to delete companies.
-// User should be able to filter companies based on criteria of the developer’s choice (e.g., by priority, location, or name).
-// Implement error handling for at least one API request
