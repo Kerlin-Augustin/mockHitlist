f96a0fad0dc591bef34f749e7502c48dbdee543d
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-03-10 23:07:47 -0400

    removed stuff

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-require('dotenv').config()
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
 app.use(cors())
